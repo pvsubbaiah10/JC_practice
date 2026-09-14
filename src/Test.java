25dfe34dc701b29404b4d83377af0a96040a3039
pvsubbaiah10 <subbaiah.psl@gmail.com>
2026-09-14 17:23:09 +0530

    14/9

--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,19 @@ public class Test {
 
 	public static void main(String[] args) {
 
-      System.out.println("RV");
+		String str = "Java is easy";
 		
 		
-
-
+	
+	
+		for(int i=str.length()-1;i>=0;i--) {
+			
+			
+		 
+			System.out.print(str.charAt(i));
+		
+		
+	}
 	}
 
 }
